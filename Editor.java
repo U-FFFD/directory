29bcc80de951fbff06e9fbdbed8776ed095b3cd7
Emmett Wesolowski <mmettej@gmail.com>
2017-03-08 16:30:08 -0600

    Update Editor.java
    made progress on adding employees,
    fixed braces.

--- Editor.java
+++ Editor.java
@@ -7,40 +7,58 @@ import java.util.Scanner;
 public class Editor {
 
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         boolean exitLoop = false;
         Scanner input = new Scanner(System.in);
 
         String entry = "";
 
         Gson g = new Gson();
-        while(!exitLoop) {
-            System.out.println("Enter a new directory record : (enter 'exit' to quit) ");
+        while (!exitLoop) {
+            System.out.println("Enter a new directory record (enter 'exit' to quit) ");
+            System.out.println("First Name: ");
+
             entry = input.nextLine();
-            if(entry.equals("exit"))
-                    exitLoop = true;
+            if (entry.equals("exit")) { break; }
+            String _fname = entry;
+
+            System.out.println("Last Name: ");
+            String _lname = input.nextLine();
+            System.out.println("Phone: ");
+            String _phone = input.nextLine();
+            System.out.println("Department: ");
+            String _dept = input.nextLine();
+
+            directoryProxy(new Employee(_fname, _lname, _phone, _dept));
 
         }
 
 
     }
 
+    public String directoryProxy(Employee e) {
+
+    }
+
+    private class Employee {
+
+        private String _fname;
+        private String _lname;
+        private String _department;
+        private String _phonenum;
+        
+        public Employee() {}
+
+        public Employee(String fname, String lname, String department, String phonenum) {
+            _fname = fname;
+            _lname = lname;
+            _department = department;
+            _phonenum = phonenum;
+        }
+
+        public String toString() {
+            return _lname + ", " + _fname + " " + _phonenum + " " + _department;
+        }
+
+    }
 }
-
-private class Employee {
-	
-	private String _fname;
-	private String _lname;
-	private String _department;
-	private String _phonenum;
-
-	public Employee(String fname, String lname, String department, String phonenum) {
-		_fname = fname;
-		_lname = lname;
-		_department = department;
-		_phonenum = phonenum;
-	}
-
-	public String toString() {
-		return _lname + ", " + _fname + " " + _phonenum + " " +  _department;
-	}
