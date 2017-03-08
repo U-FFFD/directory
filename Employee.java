eca53cdc091fb25dbe5bd875506e1dac081a02b9
Emmett Wesolowski <mmettej@gmail.com>
2017-03-08 16:46:43 -0600

    Create Employee.java
    Separated into independent class.

---
+++ Employee.java
@@ -0,0 +1,21 @@
+public class Employee {
+
+    private String _fname;
+    private String _lname;
+    private String _department;
+    private String _phonenum;
+
+    public Employee() {}
+
+    public Employee(String fname, String lname, String department, String phonenum) {
+        this._fname = fname;
+        this._lname = lname;
+        this._department = department;
+        this._phonenum = phonenum;
+    }
+
+    public String toString() {
+        return _lname + ", " + _fname + " " + _phonenum + " " + _department;
+    }
+
+}
