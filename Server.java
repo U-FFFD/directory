9e80e263a750b88860f30c61f207c861e5ead6b9
Owen Monsma <darkfire613@me.com>
2017-03-08 15:39:17 -0600

    added Server

---
+++ Server.java
@@ -0,0 +1,34 @@
+/*
+  CS361
+  Lab07
+  8 March 2017
+*/
+
+import java.util.Collection;
+
+public class Server{
+  public class MainDirectory implements Directory{
+    public Directory(){
+
+    }
+
+    public void add(){
+
+    }
+
+    public void print(){
+
+    }
+
+    public void clear(){
+
+    }
+  }
+
+  private class Employee{
+    public String lastName;
+    public String firstName;
+    public String phone;
+    public String dept;
+  }
+}
