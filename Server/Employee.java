40615554c750494df307aa52e10b0b872aa04783
Owen Monsma <darkfire613@me.com>
2017-03-15 15:19:07 -0500

    split into two directories for client and server

---
+++ Server/Employee.java
@@ -0,0 +1,29 @@
+public class Employee implements Comparable<Employee>{
+
+    private String _fname;
+    private String _lname;
+    private String _department;
+    private String _phonenum;
+
+    public Employee() {}
+
+    public Employee(String fname, String lname, String department, String phonenum) {
+        this._fname = fname;
+        this._lname = lname;
+        this._department = department;
+        this._phonenum = phonenum;
+    }
+
+    public String toString() {
+        return _lname + ", " + _fname + " " + _phonenum + " " + _department + "\n";
+    }
+
+    public int compareTo(Employee other){
+      if (this._lname.equals(other._lname)){
+        return this._fname.compareTo(other._fname);
+      }else{
+        return this._lname.compareTo(other._fname);
+      }
+    }
+
+}
