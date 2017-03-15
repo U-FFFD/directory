d6f745ec8b0ff4e7d683adc28bff6f031c736569
Emmett Wesolowski <mmettej@gmail.com>
2017-03-15 17:48:46 -0500

    Update Main.java

--- Client/Main.java
+++ Client/Main.java
@@ -1,4 +1,8 @@
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -9,6 +13,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 
 public class Main extends Application {
@@ -50,7 +55,6 @@ public class Main extends Application {
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
-        //grid.setPadding(new Insets(25, 25, 25, 25));
         grid.setTranslateY(-140);
         grid.setTranslateX(90);
 
@@ -84,20 +88,41 @@ public class Main extends Application {
 
         final ToggleGroup group = new ToggleGroup();
         RadioButton rb1 = new RadioButton("Male");
+        rb1.setTextFill(Color.WHITE);
         rb1.setToggleGroup(group);
         rb1.setSelected(true);
         rb1.setTranslateX(200);
         rb1.setTranslateY(-140);
 
         RadioButton rb2 = new RadioButton("Female");
+        rb2.setTextFill(Color.WHITE);
         rb2.setToggleGroup(group);
         rb2.setTranslateX(205);
         rb2.setTranslateY(-110);
 
         RadioButton rb3 = new RadioButton("Other");
+        rb3.setTextFill(Color.WHITE);
         rb3.setToggleGroup(group);
         rb3.setTranslateX(202);
         rb3.setTranslateY(-80);
+
+        group.selectedToggleProperty().addListener(new ChangeListener<Toggle>(){
+            public void changed(ObservableValue<? extends Toggle> ov,
+                                Toggle old_toggle, Toggle new_toggle) {
+                        System.out.println("test RB");
+
+
+            }
+        });
+
+        ListView<String> list = new ListView<>();
+        ObservableList<String> items = FXCollections.observableArrayList (
+            "Mr." , "Ms." , "Mrs." , "Dr." , "Col." , "Prof.");
+
+        list.setMaxSize(60,110);
+        list.setItems(items);
+        list.setTranslateX(-170);
+        list.setTranslateY(-110);
 
         root.getChildren().add(grid);
         root.getChildren().add(btn);
@@ -105,6 +130,7 @@ public class Main extends Application {
         root.getChildren().add(rb1);
         root.getChildren().add(rb2);
         root.getChildren().add(rb3);
+        root.getChildren().add(list);
 
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
