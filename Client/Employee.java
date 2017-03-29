3b01f0473f44a0d38c175399fc49270b338c50b3
Emmett Wesolowski <mmettej@gmail.com>
2017-03-29 16:00:43 -0500

    Add files via upload

---
+++ Client/Employee.java
@@ -0,0 +1,62 @@
+public class Employee implements Comparable<Employee>{
+
+    protected enum Gender{
+      MALE("male"),
+      FEMALE("female"),
+      OTHER("other");
+
+      protected final String str;
+
+      public String toString(){
+        return str;
+      }
+
+      Gender(String str){this.str = str;}
+    }
+
+    protected enum Title{
+      MR("Mr. "),
+      MS("Ms. "),
+      MRS("Mrs. "),
+      DR("Dr. "),
+      COL("Col. "),
+      PROF("Prof. ");
+
+      protected final String str;
+
+      public String toString(){
+        return str;
+      }
+      Title(String str){this.str = str;}
+    }
+
+    protected String _fname;
+    protected String _lname;
+    protected String _department;
+    protected String _phonenum;
+
+    protected Gender _gender;
+    protected Title _title;
+
+    public Employee() {}
+
+    public Employee(String fname, String lname, String department, String phonenum) {
+        this._fname = fname;
+        this._lname = lname;
+        this._department = department;
+        this._phonenum = phonenum;
+    }
+
+    public String toString() {
+        return _title.toString() + _fname + " " + _lname + "; " + _gender.toString() + " " + _phonenum + " " + _department + "\n";
+    }
+
+    @Override public int compareTo(Employee other){
+      if (this._lname.equals(other._lname)){
+        return this._fname.compareTo(other._fname);
+      }else{
+        return this._lname.compareTo(other._fname);
+      }
+    }
+
+}
