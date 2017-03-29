f3e04d112c55bbdf4fcebda7d6fa29bf82dadef5
Emmett Wesolowski <mmettej@gmail.com>
2017-03-29 17:42:00 -0500

    Update Main.java

--- Client/Main.java
+++ Client/Main.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -23,7 +24,7 @@ import java.net.URL;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(final Stage primaryStage) {
         String _fname;
         String _lname;
         String _department;
@@ -51,27 +52,27 @@ public class Main extends Application {
         Label firstName = new Label("First Name:");
         firstName.setTextFill(Color.WHITE);
         grid.add(firstName, 0, 1);
-        TextField firstNameField = new TextField();
+        final TextField firstNameField = new TextField();
         grid.add(firstNameField, 1, 1);
 
         Label lastName = new Label("Last Name:");
         lastName.setTextFill(Color.WHITE);
         grid.add(lastName, 0, 2);
-        TextField lastNameField = new TextField();
+        final TextField lastNameField = new TextField();
         grid.add(lastNameField, 1, 2);
 
         Label department = new Label("Department:");
         department.setTextFill(Color.WHITE);
         grid.add(department, 0,3);
-        TextField departmentField = new TextField();
+        final TextField departmentField = new TextField();
         grid.add(departmentField, 1 , 3);
 
         Label phone = new Label("Phone Number:");
         phone.setTextFill(Color.WHITE);
         grid.add(phone, 0,4);
-        TextField phoneField = new TextField();
+        final TextField phoneField = new TextField();
         grid.add(phoneField, 1 , 4);
-              
+
         final ToggleGroup group = new ToggleGroup();
         RadioButton maleRb = new RadioButton("Male");
         maleRb.setUserData("Male");
@@ -97,50 +98,53 @@ public class Main extends Application {
 
         group.selectedToggleProperty().addListener(new ChangeListener<Toggle>(){
             public void changed(ObservableValue<? extends Toggle> ov,
-                  Toggle old_toggle, Toggle new_toggle) { 
-                  System.out.println("Setting Gender");
+                                Toggle old_toggle, Toggle new_toggle) {
+                System.out.println("Setting Gender");
             }
         });
 
-        ListView<String> list = new ListView<>();
+        final ListView<String> list = new ListView<>();
         ObservableList<String> items = FXCollections.observableArrayList (
-            "Mr." , "Ms." , "Mrs." , "Dr." , "Col." , "Prof.");
+                "Mr." , "Ms." , "Mrs." , "Dr." , "Col." , "Prof.");
 
         list.setMaxSize(60,110);
         list.setItems(items);
         list.setTranslateX(-170);
         list.setTranslateY(-110);
-         
+
         Button submitBtn = new Button();
         submitBtn.setText("Submit [send]");
         submitBtn.setTranslateX(0);
-        submitBtn.setTranslateY(20); 
+        submitBtn.setTranslateY(20);
         submitBtn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
-            String _fname = firstNameField.getText();
-            String _lname = lastNameField.getText();
-            String _department = departmentField.getText();
-            String _phonenum = phoneField.getText();
-            String _gender = group.getSelectedToggle().getUserData().toString();
-       
-            Employee.Prefix _prefix = Employee.Prefix.valueOf(list.getOnMouseClicked().toString().toUpperCase());
-            System.out.println(_prefix.toString());
-            
-            	Employee e = new Employee(_fname, _lname, _department, _phonenum, _gender, _prefix);  
+                String _fname = firstNameField.getText();
+                String _lname = lastNameField.getText();
+                String _department = departmentField.getText();
+                String _phonenum = phoneField.getText();
+                String _gender = group.getSelectedToggle().getUserData().toString();
+                String fromList = list.getSelectionModel().getSelectedItem().toUpperCase();
+                Employee.Prefix _prefix = Employee.Prefix.valueOf(fromList.substring(0,fromList.length()-1));
+
+                Employee e = new Employee(_fname, _lname, _department, _phonenum, _gender, _prefix);
+                System.out.println(e);
+                Gson g = new Gson();
+                String jsonEmployee = g.toJson(e);
+                sendDataToServer(jsonEmployee);
             }
         });
-        
+
         Button exitBtn = new Button();
         exitBtn.setText("Exit");
         exitBtn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
-            	System.out.println("Exiting");
-            	primaryStage.close();
-                
+                System.out.println("Exiting");
+                primaryStage.close();
+
             }
         });
         exitBtn.setTranslateY(100);
@@ -156,8 +160,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
         
-        
-
     }
 
 
